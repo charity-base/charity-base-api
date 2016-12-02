435e628ddea3324932ec5232b5b1431c6cf3d160
Dan <dan.kwiat@gmail.com>
2016-12-02 12:33:47 +0000

    define functions for generating db query

--- api/controllers/charity-controller.js
+++ api/controllers/charity-controller.js
@@ -2,38 +2,65 @@ var mongoose = require('mongoose');
 var Charity = require('../../models/charity')(mongoose);
 
 
-module.exports.getCharities = function (req, res) {
+function generateFilter (urlQuery) {
 
-  var filter = {},
-      projection = {},
-      sort = {};
+  var filter = {};
 
+  if (urlQuery.f_charityNumber) {
+    filter.charityNumber = Number(urlQuery.f_charityNumber);
+  }
+  if (urlQuery.f_subNumber) {
+    filter.subNumber = Number(urlQuery.f_subNumber);
+  }
+  if (urlQuery.f_registeredOnly=='true') {
+    filter.registered = true;
+  }
+  if (urlQuery.f_searchTerm) {
+    var quotedWords = urlQuery.f_searchTerm.split('"').join('').split(' ').join('" "');
+    quotedWords = `"${quotedWords}"`;
+    filter["$text"] = { "$search" : quotedWords };
+  }
+
+  return filter;
+}
+
+
+function generateProjection (urlQuery) {
+  var optionalFields = ['govDoc', 'areaOfBenefit', 'mainCharity', 'contact', 'accountSubmission', 'returnSubmission', 'areaOfOperation', 'class', 'financial', 'otherNames', 'objects', 'partB', 'registration', 'trustees'];
+
+  var projection = {};
   projection._id = false;
 
-  var projFields = ['govDoc', 'areaOfBenefit', 'mainCharity', 'contact', 'accountSubmission', 'returnSubmission', 'areaOfOperation', 'class', 'financial', 'otherNames', 'objects', 'partB', 'registration', 'trustees'];
-  for (var i=0; i<projFields.length; i++) {
-    var key = 'p_'+projFields[i];
-    if (req.query[key]!='true') {
-      projection[projFields[i]] = false;
+  if (urlQuery.f_searchTerm) {
+    projection.score = { "$meta" : "textScore" };
+  }
+
+  for (var i=0; i<optionalFields.length; i++) {
+    var field = optionalFields[i];
+    var key = `p_${field}`;
+    if (urlQuery[key]!='true') {
+      projection[field] = false;
     }
   }
 
-  if (req.query.hasOwnProperty('f_charityNumber')) {
-    filter.charityNumber = Number(req.query.f_charityNumber);
+  return projection;
+}
+
+
+function generateSorting (urlQuery) {
+  var sorting = {};
+  if (urlQuery.f_searchTerm) {
+    sorting.score = { "$meta" : "textScore" };
   }
-  if (req.query.hasOwnProperty('f_subNumber')) {
-    filter.subNumber = Number(req.query.f_subNumber);
-  }
-  if (req.query.f_registeredOnly=='true') {
-    filter.registered = true;
-  }
-  if (req.query.f_searchTerm) {
-    var quotedWords = req.query.f_searchTerm.split('"').join('').split(' ').join('" "');
-    quotedWords = '"' + quotedWords + '"';
-    filter["$text"] = { "$search" : quotedWords };
-    projection["score"] =  { "$meta" : "textScore" };
-    sort["score"] = { "$meta" : "textScore" };
-  }
+  return sorting;
+}
+
+
+module.exports.getCharities = function (req, res) {
+
+  var filter = generateFilter(req.query);
+  var projection = generateProjection(req.query);
+  var sorting = generateSorting(req.query);
 
   var nPerPage = 10;
   var pageNumber = req.query.l_pageNumber>0 ? req.query.l_pageNumber : 1;
@@ -42,7 +69,7 @@ module.exports.getCharities = function (req, res) {
     if (err1) {
       return res.status(400).send({message: err1});
     }
-    Charity.find(filter, projection).sort(sort).skip((pageNumber-1)*nPerPage).limit(nPerPage).exec(function (err2, charities) {
+    Charity.find(filter, projection).sort(sorting).skip((pageNumber-1)*nPerPage).limit(nPerPage).exec(function (err2, charities) {
       if (err2) {
         return res.status(400).send({message: err2});
       }
