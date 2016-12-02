471096fb72905340de0356e00e093f15a9b3fc6b
Dan <dan.kwiat@gmail.com>
2016-12-02 12:34:19 +0000

    add some comments

--- api/controllers/charity-controller.js
+++ api/controllers/charity-controller.js
@@ -7,15 +7,20 @@ function generateFilter (urlQuery) {
   var filter = {};
 
   if (urlQuery.f_charityNumber) {
+    // Match specified charity number (could return multiple results if f_subNumber not specified)
     filter.charityNumber = Number(urlQuery.f_charityNumber);
   }
   if (urlQuery.f_subNumber) {
+    // Match specified subsidiary number e.g. "f_subNumber=0" to return main charities only
+    // Explanation: http://apps.charitycommission.gov.uk/Showcharity/ShowCharity_Help_Page.aspx?ContentType=Help_Constituents&SelectedLanguage=English
     filter.subNumber = Number(urlQuery.f_subNumber);
   }
   if (urlQuery.f_registeredOnly=='true') {
+    // Do not return de-registered charities
     filter.registered = true;
   }
   if (urlQuery.f_searchTerm) {
+    // Perform AND text-search on charity name
     var quotedWords = urlQuery.f_searchTerm.split('"').join('').split(' ').join('" "');
     quotedWords = `"${quotedWords}"`;
     filter["$text"] = { "$search" : quotedWords };
@@ -31,10 +36,12 @@ function generateProjection (urlQuery) {
   var projection = {};
   projection._id = false;
 
+  // If the user specified a search term, return the text-match strength so we can sort results
   if (urlQuery.f_searchTerm) {
     projection.score = { "$meta" : "textScore" };
   }
 
+  // Do not return the fields named in optionalFields unless user specified "p_fieldName=true"
   for (var i=0; i<optionalFields.length; i++) {
     var field = optionalFields[i];
     var key = `p_${field}`;
@@ -49,6 +56,7 @@ function generateProjection (urlQuery) {
 
 function generateSorting (urlQuery) {
   var sorting = {};
+  // If the user specified a search term, sort results by text-match strength
   if (urlQuery.f_searchTerm) {
     sorting.score = { "$meta" : "textScore" };
   }
