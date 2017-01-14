b8b6e01d91c92848ddbd5e08f53ae8a0d5bd7a29
Dan <dan.kwiat@gmail.com>
2017-01-14 21:31:40 +0000

    remove logic for serving https requests

--- api/server.js
+++ api/server.js
@@ -2,42 +2,16 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    fs = require('fs'),
-    https = require('https'),
     charityController = require('./controllers/charity-controller'),
     config = require('./config/config')();
 
 mongoose.connect(config.mongo.address, { config: config.mongo.config });
 
-
 // Use middleware to inject bodyparser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-if (config.ssl.runHttps) {
-  // redirect http requests
-  app.use(function(req, res, next) {
-    if(!req.secure) {
-      return res.redirect(config.baseUrl() + req.url);
-    }
-    next();
-  });
-}
-
 app.get('/api/v1/charities', charityController.getCharities);
-
-if (config.ssl.runHttps) {
-
-  var sslCreds = {
-    key: fs.readFileSync(config.ssl.privateKeyFile, 'utf8'),
-    cert: fs.readFileSync(config.ssl.certificateFile, 'utf8'),
-    ca: fs.readFileSync(config.ssl.intermediateFile, 'utf8')
-  };
-
-  https.createServer(sslCreds, app).listen(config.ssl.listenPort, function() {
-    console.log('Listening on port ' + config.ssl.listenPort);
-  });
-}
 
 app.listen(config.listenPort, function() {
   console.log('Listening on port ' + config.listenPort);
