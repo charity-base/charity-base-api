42c091d77bbfebe62e9f38ec2331dc95a75597ce
Dan <dan.kwiat@gmail.com>
2017-01-14 21:52:08 +0000

    change default port to 3000

--- api/config/config.js
+++ api/config/config.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   return {
-    listenPort: 80,
+    listenPort: 3000,
     mongo: {
       address: "mongodb://localhost:27017/open-charities",
       config: { autoIndex: true }
