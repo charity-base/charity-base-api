e864e55e0224d135e67e768f9e5d1323b271ca38
Dan <dan.kwiat@gmail.com>
2017-01-14 21:43:18 +0000

    remove bodyParser since only accepting GET requests

--- api/server.js
+++ api/server.js
@@ -1,15 +1,10 @@
 var express = require('express'),
     app = express(),
-    bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     charityController = require('./controllers/charity-controller'),
     config = require('./config/config')();
 
 mongoose.connect(config.mongo.address, { config: config.mongo.config });
-
-// Use middleware to inject bodyparser
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 app.get('/api/v1/charities', charityController.getCharities);
 
