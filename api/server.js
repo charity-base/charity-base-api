f4a3947ca01dae688f0849b3e5c4376126908726
Dan <dan.kwiat@gmail.com>
2017-01-25 09:05:35 +0000

    add welcome message to top level root /

--- api/server.js
+++ api/server.js
@@ -7,6 +7,26 @@ var express = require('express'),
 mongoose.connect(config.mongo.address, { config: config.mongo.config });
 mongoose.Promise = global.Promise;
 
+var homeTemplate = `
+  <!DOCTYPE html>
+  <div>
+    Congratulations, you've made an API!
+  </div>
+  <div>
+    The main endpoint is
+    <a href="/api/v1/charities">/api/v1/charities</a>
+  </div>
+  <div>
+    Take a look at the
+    <a href="https://github.com/tithebarn/open-charities/blob/master/api/README.md">README</a>
+    for more information.
+  </div>
+`;
+
+app.get('/', function(req, res) {
+  res.send(homeTemplate);
+});
+
 app.get('/api/v1/charities', charityController.getCharities);
 
 app.listen(config.listenPort, function() {
