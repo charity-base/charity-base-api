b356baf3d5e6f3554a957682433cea3f2d400bee
dan-kwiat <dan.kwiat@gmail.com>
2018-02-22 11:46:43 +0000

    Connect to db before listening for requests

--- helpers/connectToDb.js
+++ helpers/connectToDb.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
 
 const connectToDb = (address, config) => {
-  mongoose.connect(address, config);
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', () => {
-    console.log('Successfully connected to db');
+  return new Promise((resolve, reject) => {
+    mongoose.connect(address, config).then(resolve, reject);
   });
 }
 
