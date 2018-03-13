29332e77200d8f007119cb210c91ef837c886e95
dan-kwiat <dan.kwiat@gmail.com>
2018-03-13 12:40:09 +0000

    Move query persisting to middleware

--- routers/charities.js
+++ routers/charities.js
@@ -1,11 +1,44 @@
-const charityRouter = require('express').Router();
-const getCharities = require('../controllers/charity-controller'),
+const charityRouter = require('express').Router()
+const Charity = require('../models/charity')
+const log = require('../helpers/logger')
 
-getCharityRouter = () => {
+getCharityRouter = version => {
 
-  charityRouter.get('/', getCharities);
+  charityRouter.get('/', (req, res, next) => {
 
-  return charityRouter;
+    const { query } = res.locals
+    const countResults = req.query.hasOwnProperty('countResults')
+
+    return Promise.resolve(countResults ? (
+      Charity.count(query.filter).exec()
+    ) : null)
+    .then(count => {
+      return Promise.all([
+        count,
+        Charity
+        .find(query.filter)
+        .select(query.projection)
+        .sort(query.sort)
+        .skip(query.skip)
+        .limit(query.limit)
+        .exec()
+      ])
+    })
+    .then(([count, charities]) => {
+      return res.json({
+        totalMatches : count,
+        version,
+        query,
+        charities,
+      })
+    })
+    .catch(err => {
+      log.error(err)
+      return res.status(400).send(err)
+    })
+  })
+
+  return charityRouter
 }
 
-module.exports = getCharityRouter;
+module.exports = getCharityRouter
--- routers/index.js
+++ routers/index.js
@@ -3,14 +3,16 @@ const getCharitiesRouter = require('./charities')
 const getDownloadCharitiesRouter = require('./download-charities')
 const verifyValidVersion = require('../middlewares/verifyValidVersion')
 const parseQuery = require('../middlewares/parseQuery')
+const persistQuery = require('../middlewares/persistQuery')
 
 const getApiRouter = version => {
 
   apiRouter.use(verifyValidVersion(version))
 
   apiRouter.use(parseQuery)
+  apiRouter.use(persistQuery)
 
-  apiRouter.use('/charities', getCharitiesRouter())
+  apiRouter.use('/charities', getCharitiesRouter(version))
   
   apiRouter.use('/download-charities', getDownloadCharitiesRouter())
 
