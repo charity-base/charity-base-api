828d5f6d1b2fa67877204cfb262f5fe2509e101d
dan-kwiat <dan.kwiat@gmail.com>
2018-03-13 12:40:09 +0000

    Move query persisting to middleware

--- middlewares/persistQuery.js
+++ middlewares/persistQuery.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const Charity = require('../models/charity')
-const log = require('../helpers/logger')
 
 const LATEST_VERSION = 'v0.3.0'
 
@@ -42,41 +40,13 @@ const persistRequest = (url, query, countResults, version) => {
   })
 }
 
-
-const getCharities = (req, res, next) => {
-
+const persistQuery = (req, res, next) => {
   const { query } = res.locals
   const countResults = req.query.hasOwnProperty('countResults')
 
   persistRequest(req.url, query, countResults, LATEST_VERSION)
 
-  return Promise.resolve(countResults ? (
-    Charity.count(query.filter).exec()
-  ) : null)
-  .then(count => {
-    return Promise.all([
-      count,
-      Charity
-      .find(query.filter)
-      .select(query.projection)
-      .sort(query.sort)
-      .skip(query.skip)
-      .limit(query.limit)
-      .exec()
-    ])
-  })
-  .then(([count, charities]) => {
-    return res.json({
-      version : LATEST_VERSION,
-      totalMatches : count,
-      query,
-      charities,
-    })
-  })
-  .catch(err => {
-    log.error(err)
-    return res.status(400).send(err)
-  })
+  return next()
 }
 
-module.exports = getCharities
+module.exports = persistQuery
