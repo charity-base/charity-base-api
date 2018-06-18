e741345eb1f4869a7f329342f700a7336cfd1e6f
dan-kwiat <dan.kwiat@gmail.com>
2018-06-18 13:15:42 +0100

    Temporary hack to allow searching charity numbers

--- middlewares/parse-query/index.js
+++ middlewares/parse-query/index.js
@@ -2,9 +2,21 @@ const parseSearch = require('./search')
 const parseFilter = require('./filter')
 const parseMeta = require('./meta')
 
+const intercept = query => {
+  const searchNumber = Number(query['search'])
+  if (searchNumber && searchNumber > 200000) {
+    query['ids.GB-CHC'] = query['search']
+    query['search'] = undefined
+  }
+}
+
 const getQuery = () => (req, res, next) => {
 
   const { query } = req
+
+  // Temporary hack to allow searching charity numbers
+  // TODO: add string field for raw charity numbers instead
+  intercept(query)
 
   const must = parseSearch(query)
   const filter = parseFilter(query)
