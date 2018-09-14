4fc793bec9a7a3ae041a8ba18a4a7681bb0f4a41
dan-kwiat <dan.kwiat@gmail.com>
2018-09-14 14:24:05 +0100

    Add to allowed fields for download

--- routers/download/constants.js
+++ routers/download/constants.js
@@ -8,6 +8,16 @@ const ALLOWED_CSV_FIELD_PATHS = [
   'contact.address',
   'contact.geo.longitude',
   'contact.geo.latitude',
+  'people.volunteers',
+  'people.employees',
+  'people.trustees',
+  'activities',
+  'website',
+  'income.latest.total',
+  'areaOfBenefit',
+  'causes',
+  'beneficiaries',
+  'operations',
 ]
 
 module.exports = { ALLOWED_CSV_FIELD_PATHS }
