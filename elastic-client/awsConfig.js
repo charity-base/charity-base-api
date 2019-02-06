6cfacfa11c4b96183075337d8be28c223a0ae3b7
dan-kwiat <dan.kwiat@gmail.com>
2019-02-06 20:50:18 +0000

    refactor(elastic): point to aws elasticsearch domain instead of localhost

---
+++ elastic-client/awsConfig.js
@@ -0,0 +1,17 @@
+const AWS = require('aws-sdk')
+const config = require('../config')
+
+const accessKeyId = process.env.CHARITY_BASE_ES_AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.CHARITY_BASE_ES_AWS_SECRET_ACCESS_KEY
+
+const awsConfig = new AWS.Config({
+  region: config.elastic.region,
+})
+
+if (accessKeyId && secretAccessKey) {
+  awsConfig.update({
+    credentials: new AWS.Credentials(accessKeyId, secretAccessKey)
+  })
+}
+
+module.exports = awsConfig
