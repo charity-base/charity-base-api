47aa95a5d399dd37839da238a5954f9864cefe23
dan-kwiat <dan.kwiat@gmail.com>
2019-02-06 21:33:47 +0000

    refactor(mongo): point to mongo atlas host instead of localhost

--- helpers/connectToDb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.Promise = global.Promise
-
-const connectToDb = (address, config) => {
-  return mongoose.connect(address, config)
-}
-
-module.exports = connectToDb
