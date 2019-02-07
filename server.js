aaab908b8c69326b066a1e9e98cb1770668f1914
dan-kwiat <dan.kwiat@gmail.com>
2019-02-07 08:17:21 +0000

    refactor(mongo): connect & start app in parallel to avoid now deployment confusion

--- server.js
+++ server.js
@@ -11,18 +11,18 @@ log.info(`Starting process with NODE_ENV=${process.env.NODE_ENV}`)
 
 const listenPort = process.env.PORT || 4000
 
-mongooseConnection.then(() => {
-  const app = express()
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(cors())
-  app.use('/api/:version/', apiRouter(config.version, config.elastic, config.jwt))
-  app.listen(listenPort, () => {
-    log.info(`Listening on port ${listenPort}`)
-  })
-}).catch(err => {
+mongooseConnection.catch(err => {
   log.error(err)
   process.exit(1)
+})
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use('/api/:version/', apiRouter(config.version, config.elastic, config.jwt))
+app.listen(listenPort, () => {
+  log.info(`Listening on port ${listenPort}`)
 })
 
 process.on('uncaughtException', err => {
