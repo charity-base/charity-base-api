183aaef7c8cddd9950e9c46917a3666362d6d342
dan-kwiat <dan.kwiat@gmail.com>
2019-02-14 17:11:41 +0000

    feat(auth): add @isAuthenticated and @hasScopes directives to GraphQL schema

---
+++ graphql-schema/directives/index.js
@@ -0,0 +1,24 @@
+const { GraphQLError } = require('graphql')
+
+const directiveResolvers = {
+  isAuthenticated(next, source, args, context) {
+    if (!context.apiKeyValue) {
+      throw new GraphQLError(`You must supply an Authorization header of the form "Apikey my-api-key-example". See https://charitybase.uk/api-portal for more information.`)
+    }
+    if (!context.apiKeyValid) {
+      throw new GraphQLError(`The provided API key is not valid: "${context.apiKeyValue}"`)
+    }
+    return next()
+  },
+  hasScopes(next, source, args, context) {
+    const expectedScopes = args.scopes
+    const scopes = context.apiScopes
+    const hasAllScopes = scopes && expectedScopes.every(x => scopes.indexOf(x) !== -1)
+    if (!hasAllScopes) {
+      throw new GraphQLError(`You are not authorized. Expected scopes: "${expectedScopes.join(', ')}"`)
+    }
+    return next()
+  }
+}
+
+module.exports = directiveResolvers
