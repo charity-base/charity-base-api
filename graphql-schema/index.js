f5045b8a17ecf1c0ff3062497552e41af8999499
dan-kwiat <dan.kwiat@gmail.com>
2019-02-15 13:31:47 +0000

    feat(PageLimit): limit results per page to 30

--- graphql-schema/index.js
+++ graphql-schema/index.js
@@ -1,9 +1,13 @@
 const { makeExecutableSchema } = require('graphql-tools')
 const directiveResolvers = require('./directives')
+const { GraphQLInputInt } = require('graphql-input-number')
+
 
 const typeDefs = `
   directive @isAuthenticated on QUERY | FIELD_DEFINITION
   directive @hasScopes(scopes: [String]) on QUERY | FIELD_DEFINITION
+
+  scalar PageLimit
 
   input FilterCHC {
     search: String
@@ -35,7 +39,7 @@ const typeDefs = `
     """
     List of charities matching query
     """
-    list(limit: Int, skip: Int, sort: String): [CharityCHC]
+    list(limit: PageLimit, skip: Int, sort: String): [CharityCHC]
   }
 
   type QueryCHC {
@@ -53,6 +57,17 @@ const typeDefs = `
   }
 `
 
-const schema = makeExecutableSchema({ typeDefs, directiveResolvers })
+const PageLimit = GraphQLInputInt({
+  name: 'PageLimit',
+  description: 'The `PageLimit` integer type defines the number of results returned per request.  `Min`: 1, `Max`: 30.  If you want much more than this you should consider an aggregation or download query instead of list.',
+  min: 1,
+  max: 30,
+})
+
+const resolvers = {
+  PageLimit,
+}
+
+const schema = makeExecutableSchema({ typeDefs, directiveResolvers, resolvers })
 
 module.exports = schema
