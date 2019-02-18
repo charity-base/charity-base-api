c2d613b88f1374ea33730b401a35fbae67a58619
dan-kwiat <dan.kwiat@gmail.com>
2019-02-18 15:55:41 +0000

    style(errors): send dummy key example when no api key received

--- graphql/directiveResolvers/index.js
+++ graphql/directiveResolvers/index.js
@@ -3,7 +3,7 @@ const { GraphQLError } = require('graphql')
 const directiveResolvers = {
   isAuthenticated(next, source, args, context) {
     if (!context.apiKeyValue) {
-      throw new GraphQLError(`You must supply an Authorization header of the form "Apikey my-api-key-example". See https://charitybase.uk/api-portal for more information.`)
+      throw new GraphQLError(`You must supply an Authorization header of the form "Apikey 9447fa04-c15b-40e6-92b6-30307deeb5d1". See https://charitybase.uk/api-portal for more information.`)
     }
     if (!context.apiKeyValid) {
       throw new GraphQLError(`The provided API key is not valid: "${context.apiKeyValue}"`)
