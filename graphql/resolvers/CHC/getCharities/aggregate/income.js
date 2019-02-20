c63d1ddecf67151cc1b6d70daef35b63d8d27a5c
dan-kwiat <dan.kwiat@gmail.com>
2019-02-20 17:42:52 +0000

    refactor(aggregate): refactor agg resolver to prepare for adding agg types (under same es query)

--- graphql/resolvers/CHC/getCharities/aggregate/income.js
+++ graphql/resolvers/CHC/getCharities/aggregate/income.js
@@ -1,52 +1,36 @@
-const { esClient } = require('../../../../../connection')
-const config = require('../../../../../config.json')
 
-const esIndex = config.elastic.index
-
-const aggs = {
-  agg_by_income: {
-    histogram: {
-      field: 'income.latest.total',
-      script: 'Math.log10(_value)',
-      interval: 0.5,
-      extended_bounds: {
-        min: 0,
-        max: 9,
-      },
+const aggQuery = {
+  histogram: {
+    field: 'income.latest.total',
+    script: 'Math.log10(_value)',
+    interval: 0.5,
+    extended_bounds: {
+      min: 0,
+      max: 9,
     },
-    aggs: {
-      total_income: {
-        sum: {
-          field: 'income.latest.total',
-        },
+  },
+  aggs: {
+    total_income: {
+      sum: {
+        field: 'income.latest.total',
       },
     },
   },
 }
 
-async function aggByIncome(esQuery) {
-  const searchParams = {
-    index: esIndex,
-    size: 0,
-    body: {
-      query: esQuery,
-      aggs,
-    },
-  }
-  try {
-    const response = await esClient.search(searchParams)
-    const buckets = response.aggregations.agg_by_income.buckets.map(x => ({
-      id: `${x.key}`,
-      name: `Min. £${Math.round(Math.pow(10, x.key))}`,
-      count: x.doc_count,
-      sumIncome: x.total_income.value,
-    }))
-    return {
-      buckets,
-    }
-  } catch(e) {
-    throw e
+const parseResponse = aggregation => {
+  const buckets = aggregation.buckets.map(x => ({
+    id: `${x.key}`,
+    name: `Min. £${Math.round(Math.pow(10, x.key))}`,
+    count: x.doc_count,
+    sumIncome: x.total_income.value,
+  }))
+  return {
+    buckets,
   }
 }
 
-module.exports = aggByIncome
+module.exports = {
+  aggQuery,
+  parseResponse,
+}
