56dc00c82159c3bc72cbc972acaca3cd27534faa
dan-kwiat <dan.kwiat@gmail.com>
2019-02-25 12:38:26 +0000

    feat(filters): add geo bounding circle filter to getCharities

--- graphql/resolvers/CHC/getCharities/helpers/query/geo.js
+++ graphql/resolvers/CHC/getCharities/helpers/query/geo.js
@@ -1,3 +1,5 @@
+const GEO_FIELD = 'contact.geoCoords'
+
 const getGeoFilters = geo => {
   if (!geo) return []
 
@@ -6,7 +8,19 @@ const getGeoFilters = geo => {
   if (geo.boundingBox) {
     filters.push({
       geo_bounding_box: {
-        'contact.geoCoords': geo.boundingBox,
+        [GEO_FIELD]: geo.boundingBox,
+      },
+    })
+  }
+
+  if (geo.boundingCircle) {
+    filters.push({
+      geo_distance: {
+        distance: `${geo.boundingCircle.radius}${geo.boundingCircle.unit || 'mi'}`,
+        [GEO_FIELD]: {
+          lat: geo.boundingCircle.latitude,
+          lon: geo.boundingCircle.longitude,
+        },
       },
     })
   }
