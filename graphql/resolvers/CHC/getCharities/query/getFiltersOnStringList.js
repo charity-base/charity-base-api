437bd90c55c50ddce6cd171ad41c959c1411686a
dan-kwiat <dan.kwiat@gmail.com>
2019-03-04 15:37:54 +0000

    fix(filters): ensure new & old numeric range syntaxes are both respected

--- graphql/resolvers/CHC/getCharities/query/getFiltersOnStringList.js
+++ graphql/resolvers/CHC/getCharities/query/getFiltersOnStringList.js
@@ -1,3 +1,4 @@
+const getFiltersOnListLength = require('./getFiltersOnListLength')
 // Return an array of Elasticsearch filters on given field (for which each doc has a list of string values)
 // listFilterInput corresponds to type ListFilterInput defined in GraphQL typeDefs.
 const getFiltersOnStringList = (field, listFilterInput) => {
@@ -38,53 +39,7 @@ const getFiltersOnStringList = (field, listFilterInput) => {
     })
   }
 
-  if (length && !isNaN(length.lessThanInclusive)) {
-    filters.push({
-      script: {
-        script: {
-          source: `doc['${field}'].values.length <= params.value`,
-          lang: 'painless',
-          params: { value: length.lessThanInclusive },
-        }
-      }
-    })
-  }
-
-  if (length && !isNaN(length.lessThanExclusive)) {
-    filters.push({
-      script: {
-        script: {
-          source: `doc['${field}'].values.length < params.value`,
-          lang: 'painless',
-          params: { value: length.lessThanExclusive },
-        }
-      }
-    })
-  }
-
-  if (length && !isNaN(length.moreThanInclusive)) {
-    filters.push({
-      script: {
-        script: {
-          source: `doc['${field}'].values.length >= params.value`,
-          lang: 'painless',
-          params: { value: length.moreThanInclusive },
-        }
-      }
-    })
-  }
-
-  if (length && !isNaN(length.moreThanExclusive)) {
-    filters.push({
-      script: {
-        script: {
-          source: `doc['${field}'].values.length > params.value`,
-          lang: 'painless',
-          params: { value: length.moreThanExclusive },
-        }
-      }
-    })
-  }
+  getFiltersOnListLength(field, length).map(f => filters.push(f))
 
   return filters
 }
