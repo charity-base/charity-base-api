10fcfd1359650482fe550962c09da1396e2a016c
dan-kwiat <dan.kwiat@gmail.com>
2019-03-07 18:09:56 +0000

    feat(filters): add geo.region filter

--- graphql/resolvers/CHC/getCharities/query/geo.js
+++ graphql/resolvers/CHC/getCharities/query/geo.js
@@ -1,4 +1,17 @@
-const GEO_FIELD = 'contact.geoCoords'
+const GEO_REGION_FIELD = 'contact.geo.region'
+const GEO_COORDS_FIELD = 'contact.geoCoords'
+
+const geoRegionCodes = {
+  E12000001: 'North East',
+  E12000002: 'North West',
+  E12000003: 'Yorkshire and The Humber',
+  E12000004: 'East Midlands',
+  E12000005: 'West Midlands',
+  E12000006: 'East of England',
+  E12000007: 'London',
+  E12000008: 'South East',
+  E12000009: 'South West',
+}
 
 const getGeoFilters = geo => {
   if (!geo) return []
@@ -8,7 +21,7 @@ const getGeoFilters = geo => {
   if (geo.boundingBox) {
     filters.push({
       geo_bounding_box: {
-        [GEO_FIELD]: geo.boundingBox,
+        [GEO_COORDS_FIELD]: geo.boundingBox,
       },
     })
   }
@@ -17,10 +30,19 @@ const getGeoFilters = geo => {
     filters.push({
       geo_distance: {
         distance: `${geo.boundingCircle.radius}${geo.boundingCircle.unit}`,
-        [GEO_FIELD]: {
+        [GEO_COORDS_FIELD]: {
           lat: geo.boundingCircle.latitude,
           lon: geo.boundingCircle.longitude,
         },
+      },
+    })
+  }
+
+  if (geo.region) {
+    const geoRegionName = geoRegionCodes[geo.region]
+    filters.push({
+      term: {
+        [GEO_REGION_FIELD]: geoRegionName,
       },
     })
   }
