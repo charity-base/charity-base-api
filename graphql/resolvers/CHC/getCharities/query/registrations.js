864c0232fb2a90e292d54892fe873ae29d3c466f
dan-kwiat <dan.kwiat@gmail.com>
2019-03-07 17:02:58 +0000

    feat(filters): replace registrationDate with registrations.latestRegistrationDate

--- graphql/resolvers/CHC/getCharities/query/registrations.js
+++ graphql/resolvers/CHC/getCharities/query/registrations.js
@@ -1,8 +1,10 @@
 const getFiltersOnDate = require('./getFiltersOnDate')
 const REGISTRATION_DATE_FIELD = 'lastRegistrationDate'
 
-const getRegistrationDateFilters = dateRange => {
-  if (!dateRange) return []
+const getRegistrationDateFilters = registrations => {
+  if (!registrations) return []
+
+  const dateRange = registrations.latestRegistrationDate
   
   return getFiltersOnDate(REGISTRATION_DATE_FIELD, dateRange)
 }
