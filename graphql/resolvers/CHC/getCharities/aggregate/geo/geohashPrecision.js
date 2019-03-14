e0b4dcba64be6436568c3456c18a20f0f48d87e9
dan-kwiat <dan.kwiat@gmail.com>
2019-03-14 16:57:03 +0000

    feat(aggregate): set min geohash precision to 3

--- graphql/resolvers/CHC/getCharities/aggregate/geo/geohashPrecision.js
+++ graphql/resolvers/CHC/getCharities/aggregate/geo/geohashPrecision.js
@@ -1,4 +1,5 @@
-const MIN_GRID_BUCKETS = 50
+const DESIRED_GRID_COUNT = 50
+const MIN_PRECISION = 3
 const MAX_PRECISION = 10
 
 const radians = x => x*Math.PI/180
@@ -11,6 +12,7 @@ const getArea = (lat1, lat2, lonDiff) => {
 const geohashLatRange = p => {
   return 180/Math.pow(2, Math.floor(5*p/2))
 }
+
 const geohashLonRange = p => {
   return 360/Math.pow(2, Math.ceil(5*p/2))
 }
@@ -19,14 +21,14 @@ const geohashPrecision = ({ top, left, bottom, right }) => {
   const portalArea = getArea(top, bottom, right-left)
   const avgLat = 0.5*(top + bottom)
 
-  for (var p = 1; p < MAX_PRECISION; p++) {
+  for (var p = MIN_PRECISION; p < MAX_PRECISION; p++) {
     const lonRange = geohashLonRange(p)
     const latRange = geohashLatRange(p)
     const lat1 = avgLat + 0.5*latRange
     const lat2 = avgLat - 0.5*latRange
     const avgGeohashArea = getArea(lat1, lat2, lonRange)
 
-    if (MIN_GRID_BUCKETS*avgGeohashArea < portalArea) {
+    if (DESIRED_GRID_COUNT*avgGeohashArea < portalArea) {
       return p
     }
   }
