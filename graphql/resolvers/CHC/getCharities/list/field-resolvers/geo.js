09f62a8bf667db11f237acbd86a1cdc2ffc39c04
dan-kwiat <dan.kwiat@gmail.com>
2019-03-22 14:37:49 +0000

    fix(list): prevent error when geo requested but contact is null

--- graphql/resolvers/CHC/getCharities/list/field-resolvers/geo.js
+++ graphql/resolvers/CHC/getCharities/list/field-resolvers/geo.js
@@ -2,6 +2,42 @@ const ES_FIELDS = [
   'contact.geo',
 ]
 
+const nullGeo = {
+  postcode: null,
+  quality: null,
+  eastings: null,
+  northings: null,
+  country: null,
+  nhs_ha: null,
+  longitude: null,
+  latitude: null,
+  european_electoral_region: null,
+  primary_care_trust: null,
+  region: null,
+  lsoa: null,
+  msoa: null,
+  incode: null,
+  outcode: null,
+  parliamentary_constituency: null,
+  admin_district: null,
+  parish: null,
+  admin_county: null,
+  admin_ward: null,
+  ced: null,
+  ccg: null,
+  nuts: null,
+  codes: {
+    admin_district: null,
+    admin_county: null,
+    admin_ward: null,
+    parish: null,
+    parliamentary_constituency: null,
+    ccg: null,
+    ced: null,
+    nuts: null,
+  }
+}
+
 async function getList(
   searchSource,
 ) {
@@ -10,7 +46,9 @@ async function getList(
       _source: ES_FIELDS,
     }
     const response = await searchSource(searchParams)
-    return response.hits.hits.map(x => x._source.contact.geo)
+    return response.hits.hits.map(x => {
+      return x._source.contact ? x._source.contact.geo : nullGeo // todo: investigate how null contact is possible..
+    })
   } catch(e) {
     throw e
   }
