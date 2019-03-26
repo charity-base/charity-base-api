163717f2e9d91ffc746b86c9a8a517910a3f88d4
dan-kwiat <dan.kwiat@gmail.com>
2019-03-26 13:17:09 +0000

    feat(getFilters): enable fetching filter items by id

--- graphql/resolvers/CHC/getFilters/index.js
+++ graphql/resolvers/CHC/getFilters/index.js
@@ -6,42 +6,59 @@ const esIndex = config.elastic.indexes.chc.filters
 const MAX_SUGGESTIONS = 12
 
 async function getFilters({ prefix, id }) {
-  if (!prefix) return []
+  const filterIds = id && id.length > 0
+
+  if (!filterIds && !prefix) return []
+
   const searchParams = {
     index: [esIndex],
-    body: {
-      suggest: {
-        filterSuggest: {
-          prefix: prefix.toLowerCase(), 
-          completion: { 
-            field: 'suggest',
-            size: MAX_SUGGESTIONS,
-            fuzzy: {
-              fuzziness: 1,
-            },
-            contexts: {
-              filter_type: [ 
-                { context: "id", boost: 1 },
-                { context: "area", boost: 2 },
-                { context: "funder", boost: 3 },
-                { context: "cause", boost: 4 },
-                { context: "operation", boost: 4 },
-                { context: "beneficiary", boost: 4 },
-              ]
-            },
-          }
+    body: {},
+    _source: ['id', 'value', 'label', 'filterType'],
+  }
+
+  if (filterIds) {
+    searchParams.body.query = {
+      bool: {
+        should: id.map(x => ({
+          term: { id: x }
+        }))
+      }
+    }
+  } else if (prefix) {
+    searchParams.body.suggest = {
+      filterSuggest: {
+        prefix: prefix.toLowerCase(),
+        completion: {
+          field: 'suggest',
+          size: MAX_SUGGESTIONS,
+          fuzzy: {
+            fuzziness: 1,
+          },
+          contexts: {
+            filter_type: [
+              { context: "id", boost: 1 },
+              { context: "area", boost: 2 },
+              { context: "funder", boost: 3 },
+              { context: "cause", boost: 4 },
+              { context: "operation", boost: 4 },
+              { context: "beneficiary", boost: 4 },
+            ]
+          },
         }
       }
-    },
-    _source: ['id', 'value', 'label', 'filterType'],
+    }
   }
 
   try {
     const response = await esClient.search(searchParams)
-    return response.suggest.filterSuggest[0].options.map(x => ({
-      ...x._source,
-      score: x._score,
-    }))
+    if (!filterIds && prefix) {
+      return response.suggest.filterSuggest[0].options.map(x => ({
+        ...x._source,
+        score: x._score,
+      }))
+    } else {
+      return response.hits.hits.map(x => x._source)
+    }
   } catch(e) {
     throw e
   }
