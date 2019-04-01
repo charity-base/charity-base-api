cdf0e5acfe1232744103267c96b4478a1d602fe6
dan-kwiat <dan.kwiat@gmail.com>
2019-04-01 17:37:25 +0100

    feat(auth): add directive for requiring specific jwt scopes

--- graphql/directiveResolvers/index.js
+++ graphql/directiveResolvers/index.js
@@ -1,11 +1,13 @@
 const isAuthenticated = require('./isAuthenticated')
-const jwtAuth = require('./jwtAuth')
 const hasScopes = require('./hasScopes')
+const jwtAuth = require('./jwtAuth')
+const jwtScopes = require('./jwtScopes')
 
 const directiveResolvers = {
   isAuthenticated,
   hasScopes,
   jwtAuth,
+  jwtScopes,
 }
 
 module.exports = directiveResolvers
--- graphql/typeDefs/directive.js
+++ graphql/typeDefs/directive.js
@@ -1,5 +1,6 @@
 const typeDefs = `
   directive @jwtAuth on QUERY | FIELD_DEFINITION
+  directive @jwtScopes(scopes: [String]) on QUERY | FIELD_DEFINITION
   directive @isAuthenticated on QUERY | FIELD_DEFINITION
   directive @hasScopes(scopes: [String]) on QUERY | FIELD_DEFINITION
   directive @deprecated(reason: String = "No longer supported") on FIELD_DEFINITION | INPUT_FIELD_DEFINITION
