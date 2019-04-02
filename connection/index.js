104174c3c83a4cf45c6cf8be89b0f84327913c7f
dan-kwiat <dan.kwiat@gmail.com>
2019-04-02 09:06:17 +0100

    chore(files): remove deprecated mongo code

--- connection/index.js
+++ connection/index.js
@@ -1,11 +1,9 @@
 const esClient = require('./elastic')
 const dynamoClient = require('./dynamo')
-const mongooseConnection = require('./mongo')
 const s3 = require('./s3')
 
 module.exports = {
   esClient,
   dynamoClient,
-  mongooseConnection,
   s3,
 }
