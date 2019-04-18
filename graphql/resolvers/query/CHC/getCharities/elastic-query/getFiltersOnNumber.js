1acad3e11ab87be101b172ff521fca9827b90cbe
dan-kwiat <dan.kwiat@gmail.com>
2019-04-18 12:04:16 +0100

    feat(input): remove deprecated fields on NumericRangeInput

--- graphql/resolvers/query/CHC/getCharities/elastic-query/getFiltersOnNumber.js
+++ graphql/resolvers/query/CHC/getCharities/elastic-query/getFiltersOnNumber.js
@@ -1,18 +1,12 @@
 const getFiltersOnNumber = (field, numericRangeInput) => {
   if (!numericRangeInput) return []
 
-  const gte = isNaN(numericRangeInput.gte) ? (
-    isNaN(numericRangeInput.moreThanInclusive) ? undefined : numericRangeInput.moreThanInclusive
-  ) : numericRangeInput.gte
-  const gt = isNaN(numericRangeInput.gt) ? (
-    isNaN(numericRangeInput.moreThanExclusive) ? undefined : numericRangeInput.moreThanExclusive
-  ) : numericRangeInput.gt
-  const lte = isNaN(numericRangeInput.lte) ? (
-    isNaN(numericRangeInput.lessThanInclusive) ? undefined : numericRangeInput.lessThanInclusive
-  ) : numericRangeInput.lte
-  const lt = isNaN(numericRangeInput.lt) ? (
-    isNaN(numericRangeInput.lessThanExclusive) ? undefined : numericRangeInput.lessThanExclusive
-  ) : numericRangeInput.lt
+  const {
+    gte,
+    gt,
+    lte,
+    lt,
+  } = numericRangeInput
 
   return [{
     range: {
