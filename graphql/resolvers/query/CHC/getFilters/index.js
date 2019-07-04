c2a80036dff3990efc2f102f3d84c53b86e70953
dan-kwiat <dan.kwiat@gmail.com>
2019-07-04 18:40:02 +0100

    feat(getFilters): use edge_ngram tokenizer instead of completion suggester

--- graphql/resolvers/query/CHC/getFilters/index.js
+++ graphql/resolvers/query/CHC/getFilters/index.js
@@ -6,64 +6,75 @@ const esIndex = config.elastic.indexes.chc.filters
 const MAX_SUGGESTIONS = 12
 
 const FILTER_TYPES = [
-  { context: "trustee", boost: 1 },
-  { context: "id", boost: 2 },
-  { context: "area", boost: 3 },
-  { context: "funder", boost: 4 },
-  { context: "cause", boost: 5 },
-  { context: "operation", boost: 5 },
-  { context: "beneficiary", boost: 5 },
+  { id: 'trustee', weight: 1.2 },
+  { id: 'id', weight: 2 },
+  { id: 'area', weight: 3 },
+  { id: 'funder', weight: 4 },
+  { id: 'cause', weight: 5 },
+  { id: 'operation', weight: 5 },
+  { id: 'beneficiary', weight: 5 },
 ]
 
 async function getFilters({ filterType, id, search }) {
   const filterIds = id && id.length > 0
-
-  if (!filterIds && !search) return []
+  const cleanSearch = search && search.trim().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+  if (!filterIds && !cleanSearch) return []
 
   const searchParams = {
     index: [esIndex],
     body: {},
     _source: ['id', 'value', 'label', 'filterType'],
+    size: MAX_SUGGESTIONS,
+    from: 0,
   }
 
-  if (filterIds) {
-    searchParams.body.query = {
-      bool: {
-        should: id.map(x => ({
-          term: { id: x }
-        }))
-      }
-    }
-  } else if (search) {
-    searchParams.body.suggest = {
-      filterSuggest: {
-        prefix: search.trim().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ''),
-        completion: {
-          field: 'suggest',
-          size: MAX_SUGGESTIONS,
-          fuzzy: {
-            fuzziness: 1,
-          },
-          contexts: {
-            filter_type: filterType ? (FILTER_TYPES.filter(x => {
-              return filterType.indexOf(x.context) > -1
-            })) : FILTER_TYPES
-          },
-        }
-      }
+  searchParams.body.query = {
+    function_score: {
+      query: {
+        bool: {
+          must: cleanSearch ? {
+            match: {
+              suggest: {
+                query: cleanSearch,
+                operator: 'and',
+                fuzziness: 'AUTO',
+              },
+            },
+          } : null,
+          filter: [
+            {
+              bool: {
+                should: filterType ? FILTER_TYPES.filter(x => filterType.indexOf(x.id) > -1).map(x => ({
+                  term: { filterType: x.id }
+                })) : []
+              },
+            },
+            {
+              bool: {
+                should: id ? id.map(x => ({
+                  term: { id: x }
+                })) : []
+              },
+            },
+          ],
+        },
+      },
+      functions: FILTER_TYPES.map(x => ({
+        filter: {
+          term: { filterType: x.id }
+        },
+        weight: x.weight,
+      }))
     }
   }
 
   try {
     const response = await esClient.search(searchParams)
-    if (!filterIds && search) {
-      return response.suggest.filterSuggest[0].options.map(x => ({
-        ...x._source,
-        score: x._score,
-      }))
-    } else {
-      return response.hits.hits.map(x => x._source)
-    }
+    const hits = response.hits.hits.map(x => ({
+      ...x._source,
+      score: x._score,
+    }))
+    return hits
   } catch(e) {
     throw e
   }
