f72f0e7d02b6e7cb6cf8ae6fc6dfdbcf931b8187
dan-kwiat <dan.kwiat@gmail.com>
2020-04-09 22:32:01 +0100

    feat(id-filters): update id filters for new index

--- graphql/resolvers/query/CHC/getCharities/elastic-query/id.js
+++ graphql/resolvers/query/CHC/getCharities/elastic-query/id.js
@@ -1,16 +1,13 @@
+const ID_FIELD = 'chcId'
+
 const getIdFilters = id => {
   if (!id || !id.length) return []
-  const idInts = id.map(x => parseInt(x)).filter(x => !isNaN(x))
-  if (idInts.length === 0) {
-    // all values provided were of invalid form, return none
-    return [{
-      match_none: {}
-    }]
-  }
   return [{
     bool: {
-      should: idInts.map(id => ({
-        term: { "ids.GB-CHC": id }
+      should: id.map(x => ({
+        term: {
+          [ID_FIELD]: x
+        }
       }))
     }
   }]
