12ef299f778b75d50627055a681ddfa7c8566cf9
dan-kwiat <dan.kwiat@gmail.com>
2020-04-09 22:41:54 +0100

    feat(trustee-filters): update trustee filters for new index

--- graphql/resolvers/query/CHC/getCharities/elastic-query/trustees.js
+++ graphql/resolvers/query/CHC/getCharities/elastic-query/trustees.js
@@ -1,7 +1,8 @@
 const getFiltersOnStringList = require('./getFiltersOnStringList')
+const TRUSTEE_ID_FIELD = 'trustees.id'
 
 const getTrusteesFilters = trustees => getFiltersOnStringList(
-  'trustees.objects.id',
+  TRUSTEE_ID_FIELD,
   trustees,
 )
 
