26a71a742fbc1b49fdc339ea01d596814aa6b61b
dan-kwiat <dan.kwiat@gmail.com>
2020-04-11 13:05:15 +0100

    fix(aggregate): use distinct finance agg names to allow combining multiple agg queries under same ES request

--- graphql/resolvers/query/CHC/getCharities/aggregate/finances/agg.js
+++ graphql/resolvers/query/CHC/getCharities/aggregate/finances/agg.js
@@ -1,45 +1,49 @@
-async function agg(search, fieldName) {
-  const searchParams = {
-    index: undefined, // this is set when queries combined in parent class
-    body: {
-      query: undefined, // this is set when queries combined in parent class
-      aggs: {
-        agg1: {
-          histogram: {
-            field: fieldName,
-            script: 'Math.log10(_value)',
-            interval: 0.5,
-            extended_bounds: {
-              min: 0,
-              max: 9,
-            },
-          },
-          aggs: {
-            total: {
-              sum: {
-                field: fieldName,
+const agg = (aggName, esField) => {
+  return async (search) => {
+    const searchParams = {
+      index: undefined, // this is set when queries combined in parent class
+      body: {
+        query: undefined, // this is set when queries combined in parent class
+        aggs: {
+          [aggName]: {
+            histogram: {
+              field: esField,
+              script: 'Math.log10(_value)',
+              interval: 0.5,
+              extended_bounds: {
+                min: 0,
+                max: 9,
               },
             },
-          },
+            aggs: {
+              total: {
+                sum: {
+                  field: esField,
+                },
+              },
+            },
+          }
         }
-      }
-    },
-    size: 0,
-  }
-
-  try {
-    const response = await search(searchParams)
-    const buckets = response.aggregations.agg1.buckets.map(x => ({
-      key: `${x.key}`,
-      name: `Min. £${Math.round(Math.pow(10, x.key))}`,
-      count: x.doc_count,
-      sum: x.total.value,
-    }))
-    return {
-      buckets,
+      },
+      size: 0,
     }
-  } catch(e) {
-    throw e
+
+    try {
+      const response = await search(searchParams)
+      const buckets = response.aggregations[aggName].buckets
+        .map(x => ({
+          key: `${x.key}`,
+          name: `Min. £${Math.round(Math.pow(10, x.key))}`,
+          count: x.doc_count,
+          sum: x.total.value,
+        }))
+        .filter(x => x.key !== 'NaN') // to deal with negative spending (bad data from CC)
+      return {
+        buckets,
+      }
+    } catch(e) {
+      throw e
+    }
   }
 }
 
--- graphql/resolvers/query/CHC/getCharities/aggregate/finances/index.js
+++ graphql/resolvers/query/CHC/getCharities/aggregate/finances/index.js
@@ -3,10 +3,13 @@ const agg = require('./agg')
 const ES_FIELD_INCOME = 'finances.latest.income'
 const ES_FIELD_SPENDING = 'finances.latest.spending'
 
+const aggIncome = agg('agg_latest_income', ES_FIELD_INCOME)
+const aggSpending = agg('agg_latest_spending', ES_FIELD_SPENDING)
+
 function aggFinances(search) {
   return {
-    latestIncome: () => agg(search, ES_FIELD_INCOME),
-    latestSpending: () => agg(search, ES_FIELD_SPENDING),
+    latestIncome: () => aggIncome(search),
+    latestSpending: () => aggSpending(search),
   }
 }
 
