74df02ff366931bd88971e07cc4568d135c2e974
dan-kwiat <dan.kwiat@gmail.com>
2020-06-27 18:50:20 +0100

    fix(elastic-query): prevent undefined values filtering number ranges

--- graphql-schema/resolvers/query/CHC/getCharities/elastic-query/getFiltersOnNumber.js
+++ graphql-schema/resolvers/query/CHC/getCharities/elastic-query/getFiltersOnNumber.js
@@ -1,17 +1,13 @@
 const getFiltersOnNumber = (field, numericRangeInput) => {
   if (!numericRangeInput) return []
+  if (Object.keys(numericRangeInput).length === 0) return []
 
   const { gte, gt, lte, lt } = numericRangeInput
 
   return [
     {
       range: {
-        [field]: {
-          gte,
-          gt,
-          lte,
-          lt,
-        },
+        [field]: { gte, gt, lte, lt },
       },
     },
   ]
