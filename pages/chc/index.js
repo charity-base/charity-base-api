ca45b22ca3bbdf037681f7f9baa58e6f0722b685
dan-kwiat <dan.kwiat@gmail.com>
2020-06-27 18:49:20 +0100

    feat(filters): implement income filter using router.push to trigger server render

--- pages/chc/index.js
+++ pages/chc/index.js
@@ -1,8 +1,94 @@
+import { useState, useCallback } from "react"
+import PropTypes from "prop-types"
 import Head from "next/head"
+import { useRouter } from "next/router"
 import getCharitiesList from "web-lib/getCharitiesList"
 import CharityList from "components/CharityList"
 
-export default function ({ count, list }) {
+function stringToInt(str) {
+  if (typeof str === "undefined") return undefined
+  const int = parseInt(str)
+  if (isNaN(int)) return undefined
+  return int
+}
+
+function IntegerRange({ initial, onChange }) {
+  const [[min, max], setState] = useState(initial)
+  const update = useCallback(() => {
+    onChange([min, max].map(stringToInt))
+  }, [min, max])
+
+  const inputClassName = "w-24 px-1 text-sm border rounded"
+  return (
+    <div className="flex text-gray-700">
+      <input
+        className={inputClassName}
+        value={min || ""}
+        placeholder="Min"
+        onChange={(e) =>
+          setState([e.target.value === "" ? undefined : e.target.value, max])
+        }
+        onBlur={update}
+      />
+      <span className="px-1">-</span>
+      <input
+        className={inputClassName}
+        value={max || ""}
+        placeholder="Max"
+        onChange={(e) =>
+          setState([min, e.target.value === "" ? undefined : e.target.value])
+        }
+        onBlur={update}
+      />
+    </div>
+  )
+}
+IntegerRange.propTypes = {
+  initial: PropTypes.array,
+  onChange: PropTypes.func.isRequired,
+}
+IntegerRange.defaultProps = {
+  initial: [undefined, undefined],
+}
+
+function Filters({ finances }) {
+  const router = useRouter()
+  const latestIncome =
+    finances && finances.latestIncome ? finances.latestIncome : {}
+
+  const update = useCallback((f) => {
+    router.push({
+      pathname: router.pathname,
+      query: {
+        ...router.query,
+        filters: JSON.stringify(f || {}),
+      },
+    })
+  }, [])
+
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold my-2">Filters</h2>
+      <div className="space-y-2">
+        <div>
+          <h3 className="font-semibold">Income</h3>
+          <IntegerRange
+            initial={[latestIncome.gte, latestIncome.lt]}
+            onChange={([gte, lt]) => {
+              update({
+                finances: {
+                  latestIncome: { gte, lt },
+                },
+              })
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default function ({ count, list, filters }) {
   return (
     <div className="max-w-5xl mx-auto flex px-2 sm:px-4 md:px-8">
       <Head>
@@ -10,8 +96,8 @@ export default function ({ count, list }) {
         <meta property="og:title" content="CharityBase Search" key="title" />
         <meta name="Description" content="CharityBase England & Wales Search" />
       </Head>
-      <aside className="flex-shrink-0 py-24 pr-8 xl:pr-16 sticky top-0 h-screen overflow-auto bg-gray-100">
-        <h2 className="text-3xl">Filters</h2>
+      <aside className="flex-shrink-0 py-24 pr-8 xl:pr-16 sticky top-0 h-screen overflow-auto">
+        <Filters {...filters} />
       </aside>
       <div className="py-24">
         <div>{count} charities</div>
@@ -34,6 +120,7 @@ export async function getServerSideProps({ query }) {
   })
   return {
     props: {
+      filters,
       count,
       list,
     },
