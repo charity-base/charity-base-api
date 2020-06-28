cd9c5ed66f56dee89dc7cbd4ffd8501f581b7307
dan-kwiat <dan.kwiat@gmail.com>
2020-06-28 11:41:27 +0100

    feat(filters): add error message popup to IntegerRange

--- components/IntegerRange.js
+++ components/IntegerRange.js
@@ -5,25 +5,48 @@ function stringToInt(str) {
   if (typeof str === "undefined") return undefined
   const int = parseInt(str)
   if (isNaN(int)) return undefined
+  if (Math.abs(int) > 2 * Math.pow(10, 9)) return undefined
   return int
+}
+
+function hasError(valuesArr) {
+  return valuesArr.reduce((agg, x) => {
+    if (agg) return true
+    if (typeof x === "undefined") return false
+    return typeof stringToInt(x) === "undefined"
+  }, false)
 }
 
 export default function IntegerRange({ initial, onChange }) {
   const [[min, max], setState] = useState(initial)
+  const [error, setError] = useState(false)
 
-  useEffect(() => {
-    setState(initial)
-  }, [JSON.stringify(initial)])
+  useEffect(
+    () => {
+      setState(initial)
+      setError(hasError(initial))
+    },
+    [JSON.stringify(initial)],
+    setState,
+    setError
+  )
 
   const update = () => {
-    onChange([min, max].map(stringToInt))
+    if (hasError([min, max])) {
+      setError(true)
+    } else {
+      const clean = [min, max].map(stringToInt)
+      setError(false)
+      setState(clean)
+      onChange(clean)
+    }
   }
 
   const inputClassName =
     "w-24 px-1 text-sm border rounded focus:outline-none focus:border-gray-700 focus:shadow"
   return (
     <div
-      className="flex text-gray-700"
+      className="relative flex text-gray-700"
       onKeyDown={(e) => {
         if (e.key === "Enter") {
           update()
@@ -32,7 +55,7 @@ export default function IntegerRange({ initial, onChange }) {
     >
       <input
         className={inputClassName}
-        value={min || ""}
+        value={typeof min === "undefined" ? "" : min}
         placeholder="Minimum"
         onChange={(e) =>
           setState([e.target.value === "" ? undefined : e.target.value, max])
@@ -42,18 +65,25 @@ export default function IntegerRange({ initial, onChange }) {
       <span className="px-1 text-gray-500">-</span>
       <input
         className={inputClassName}
-        value={max || ""}
+        value={typeof max === "undefined" ? "" : max}
         placeholder="Maximum"
         onChange={(e) =>
           setState([min, e.target.value === "" ? undefined : e.target.value])
         }
         onBlur={update}
       />
+      {error ? (
+        <div className="absolute bottom-0 left-0 right-0 transform translate-y-full">
+          <div className="bg-red-100 rounded shadow text-xs text-red-500 mt-1 py-1 px-2">
+            Must be integers ≤ 2 &#215; 10<sup>9</sup>
+          </div>
+        </div>
+      ) : null}
     </div>
   )
 }
 IntegerRange.propTypes = {
-  initial: PropTypes.array,
+  initial: PropTypes.arrayOf(PropTypes.number),
   onChange: PropTypes.func.isRequired,
 }
 IntegerRange.defaultProps = {
