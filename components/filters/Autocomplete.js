a917f10a62a2d752b4509ee86ead28829cb9f98e
dan-kwiat <dan.kwiat@gmail.com>
2020-06-29 08:33:47 +0100

    feat(web-filters): add placeholder autocomplete filters

---
+++ components/filters/Autocomplete.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types"
+
+export default function Autocomplete({ title }) {
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <h3 className="text-gray-700 font-semibold my-2">{title}</h3>
+      </div>
+      <div className="relative flex text-gray-700">
+        <input
+          className="w-full px-1 text-sm border rounded opacity-50"
+          placeholder="Start typing..."
+          disabled
+        />
+      </div>
+    </div>
+  )
+}
+Autocomplete.propTypes = {
+  title: PropTypes.string.isRequired,
+}
--- components/filters/index.js
+++ components/filters/index.js
@@ -1,21 +1,21 @@
-import { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { useRouter } from "next/router"
+import Autocomplete from "./Autocomplete"
 import IntegerRange from "./IntegerRange"
 import { constructFilters, deconstructFilters } from "./helpers"
 
-export default function Filters(filters) {
+export default function Filters({ finances, grants }) {
   const router = useRouter()
-  const [{ incomeRange, spendingRange, fundersRange }, setState] = useState(
-    deconstructFilters(filters)
-  )
+  const flatFilters = deconstructFilters({ finances, grants })
 
-  useEffect(() => {
+  function updateQuery(key, value) {
+    const changed = JSON.stringify(flatFilters[key]) !== JSON.stringify(value)
+    if (!changed) return
+
     const filtersString = JSON.stringify(
       constructFilters({
-        incomeRange,
-        spendingRange,
-        fundersRange,
+        ...flatFilters,
+        [key]: value,
       })
     )
     const query = filtersString === "{}" ? {} : { filters: filtersString }
@@ -24,42 +24,59 @@ export default function Filters(filters) {
       query,
     })
     window.scrollTo(0, 0)
-  }, [...incomeRange, ...spendingRange, ...fundersRange])
+  }
 
   return (
     <div className="p-3 border border-gray-400 rounded">
       <div className="space-y-3">
         <IntegerRange
           title="Income (£)"
-          initial={incomeRange}
-          onChange={(incomeRange) => {
-            setState((x) => ({
-              ...x,
-              incomeRange,
-            }))
+          initial={flatFilters.incomeRange}
+          onChange={(x) => {
+            updateQuery("incomeRange", x)
           }}
         />
         <IntegerRange
           title="Spending (£)"
-          initial={spendingRange}
-          onChange={(spendingRange) => {
-            setState((x) => ({
-              ...x,
-              spendingRange,
-            }))
+          initial={flatFilters.spendingRange}
+          onChange={(x) => {
+            updateQuery("spendingRange", x)
           }}
         />
         <IntegerRange
           title="Number of Funders"
-          initial={fundersRange}
-          onChange={(fundersRange) => {
-            setState((x) => ({
-              ...x,
-              fundersRange,
-            }))
+          initial={flatFilters.fundersRange}
+          onChange={(x) => {
+            updateQuery("fundersRange", x)
           }}
         />
+        <Autocomplete title="Funder" />
+        <Autocomplete title="County" />
+        <Autocomplete title="Local Authority" />
+        <Autocomplete title="Areas" />
+        <Autocomplete title="Causes" />
+        <Autocomplete title="Operations" />
+        <Autocomplete title="Beneficiaries" />
+        <Autocomplete title="Trustees" />
+        <Autocomplete title="Themes" />
       </div>
     </div>
   )
+}
+
+const rangeType = PropTypes.shape({
+  gte: PropTypes.number,
+  lt: PropTypes.number,
+})
+
+Filters.propTypes = {
+  finances: PropTypes.shape({
+    latestIncome: rangeType,
+    latestSpending: rangeType,
+  }),
+  grants: PropTypes.shape({
+    funders: PropTypes.shape({
+      length: rangeType,
+    }),
+  }),
 }
